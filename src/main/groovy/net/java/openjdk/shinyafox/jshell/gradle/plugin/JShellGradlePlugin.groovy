365064a36cd94b93794c805f1bdf02bcecd95721
bitter_fox <bitterfoxc@gmail.com>
2017-03-18 21:22:33 +0900

    Fix

--- src/main/groovy/net/java/openjdk/shinyafox/jshell/gradle/plugin/JShellGradlePlugin.groovy
+++ src/main/groovy/net/java/openjdk/shinyafox/jshell/gradle/plugin/JShellGradlePlugin.groovy
@@ -1,29 +1,29 @@
 package net.java.openjdk.shinyafox.jshell.gradle.plugin
 
+import java.io.File
 import jdk.internal.jshell.tool.JShellToolProvider
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.Exec
 import org.gradle.api.tasks.compile.JavaCompile
+
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class JShellGradlePlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
         project.tasks.create('jshell')
-        .dependsOn("compileJava")
+        .dependsOn("classes")
         .doLast {
             def path
             project.tasks.withType(JavaCompile) {
-                path = classpath.asPath;
+                path = classpath.asPath
             }
 
             Thread.currentThread().setContextClassLoader(ClassLoader.getSystemClassLoader()) // promote class loader
-            JShellToolProvider.main((String[])[
-                    "--class-path", path,
-                    "-C-classpath", "-C"+path
-            ].toArray());
+            JShellToolProvider.main((String[])["--class-path", path].toArray());
         }
     }
 
